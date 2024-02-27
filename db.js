67f429733f560c5b094adf9418d53fbee9aace76
Frederic Huang <huangfrederic2002@gmail.com>
2024-02-27 15:31:20 +0100

    second commit

--- db.js
+++ db.js
@@ -2,11 +2,8 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+  process.env.DB_CONNECTION_STRING,
   {
-    host: '', // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
